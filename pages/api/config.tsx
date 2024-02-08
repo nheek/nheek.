fd69967d5e51f75457419d59674612bc00783163
nheek <nickjameshipol@gmail.com>
2024-02-08 21:42:01 +0100

    updated config to properly connect to database now

--- pages/api/config.tsx
+++ pages/api/config.tsx
@@ -1,13 +1,14 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 
+// Create a MySQL pool using environment variables
 const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT,
+  connectionLimit: 10,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT
 });
 
 export default pool;
