691aa8e8517759f1c3a0268b0a67c4236c1a05d7
nheek <nickjameshipol@gmail.com>
2024-03-05 00:26:57 +0100

    try adding level-based colors in skills

--- components/skills.tsx
+++ components/skills.tsx
@@ -17,17 +17,18 @@ export default function Skills() {
   let textsMap = getTextsMap(domainPairs);
 
   let skills = [
-    "AstroJS",
-    "CSS", "CloudFlare", "Django",
-    "Docker", "Docker Compose",
-    "ExpressJS", "Git", "GitHub", "GitHub Actions",
-    "HTML", "Java", "JavaScript", "jQuery", "MySQL",
-    "NextJS",
-    "NGINX", "NodeJS", "PHP", "Python", "PyQt",
-    "ReactJS", "RemixJS", "REST API", "SCSS", "Socket.IO",
-    "TailwindCSS", "TypeScript", "WordPress",
-    "Zola"
+    "CSS", "CloudFlare", "Django", "Zola", "Firebase",
+    "Docker", "Docker Compose", "TailwindCSS", "TypeScript", 
+    "ExpressJS", "GitHub", "GitHub Actions", "WordPress",
+    "HTML", "Java", "JavaScript", "jQuery", "MySQL", "NextJS",
+    "NGINX", "NodeJS", "PHP", "Python", "PyQT",
+    "ReactJS", "RemixJS", "RESTful API", "Socket.IO"
   ];
+  let orangeSkills = [
+    "AstroJS", "SCSS", "Swift", "Flutter", "Git", "Go", "C++"
+  ]
+  skills.sort();
+  orangeSkills.sort();
 
   return (
     <div className="px-4 pt-[25%] md:pt-[15%] min-h-max">
@@ -38,7 +39,12 @@ export default function Skills() {
           <ul className="flex flex-wrap gap-3">
             {
               skills.map((skill, index) => (
-                <li key={index} className="hover:bg-gray-200 hover:text-blue-950 px-3 py-1 border-2 border-gray-200 rounded-3xl duration-500">{skill}</li>
+                <li key={index} className="bg-green-600 px-3 py-1 rounded-3xl duration-500">{skill}</li>
+              ))
+            }
+            {
+              orangeSkills.map((skill, index) => (
+                <li key={index} className="bg-yellow-600 px-3 py-1 rounded-3xl duration-500">{skill}</li>
               ))
             }
           </ul>
