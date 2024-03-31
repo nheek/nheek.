7f00cbbbcf62717057d71ae70db2cc55758277c5
nheek <nickjameshipol@gmail.com>
2024-04-01 01:19:41 +0200

    added mini spotify playlists and removed "bug" on shareme

--- components/fun/main.tsx
+++ components/fun/main.tsx
@@ -1,12 +1,14 @@
 import dynamic from 'next/dynamic';
 const FeaturedSongs = dynamic(() => import('./featured-songs'));
 const Spotify = dynamic(() => import('./spotify'));
+const MiniSpotify = dynamic(() => import('./mini-spotify'));
 
 export default function Main() {
   return (
     <main>
         <FeaturedSongs />
         <Spotify />
+        <MiniSpotify />
     </main>
   );
 }
---
+++ components/fun/mini-spotify.tsx
@@ -0,0 +1,55 @@
+import getTextsMap from '../get-texts-map';
+
+export default function MiniSpotify() {
+  const mini_spotify = [
+    { 
+      name: 'easter playlist',
+      code:  `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/14afZbypSYEC10CXa3OhD2?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`
+    },
+  ]
+
+  const mini_spotify_no = [
+    { 
+      name: 'påske spilleliste',
+      code:  `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/14afZbypSYEC10CXa3OhD2?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`
+    },
+  ]
+
+  const wwwNheekNo = {
+    txtSpotify: 'mini spillelister',
+    links: mini_spotify_no,
+  };
+  
+  const wwwDefault = {
+    txtSpotify: 'mini playlists',
+    links: mini_spotify,
+  };
+  
+  const domainPairs = {
+    "www.nheek.no": wwwNheekNo, 
+    default: wwwDefault
+  }
+
+  let textsMap = getTextsMap(domainPairs);
+
+  return (
+    <section className="px-4 pt-[25%] md:pt-[15%] min-h-max">
+        <hgroup className="text-4xl md:text-[4rem] xl:text-[8rem]">
+          {textsMap.txtSpotify}
+        </hgroup>
+        <div className="flex flex-wrap gap-8 text-lg w-full md:w-[90%] mt-16 m-auto leading-8">
+            {
+              textsMap.links.map((spotifyItem, index) => (
+                <div
+                  key={index} 
+                  className="w-full"
+                >
+                  <h2>{spotifyItem.name}</h2>
+                  <div dangerouslySetInnerHTML={{ __html: spotifyItem.code }} />
+                </div>
+              ))
+            }
+        </div>
+    </section>
+  );
+}
