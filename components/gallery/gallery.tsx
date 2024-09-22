d032a43798bf57690ced78cb874d44c8a75d1f51
nheek <nickjameshipol@gmail.com>
2024-09-22 23:21:31 +0200

    added many pictures to gallery

--- components/gallery/gallery.tsx
+++ components/gallery/gallery.tsx
@@ -1,6 +1,23 @@
 import ImageLoader from '../utils/ImageLoader';
 
 const images = [
+  { src: "https://i.imgur.com/E9TNwuK.png", alt: "taylor swift night club lezgooo"},
+  { src: "https://i.imgur.com/QXoIOl7.png", alt: "cgi ducks based"},
+  { src: "https://i.imgur.com/ThSuFiB.jpeg", alt: "that is not my trash trust me"},
+  { src: "https://i.imgur.com/u7ZtkEg.jpeg", alt: "this coat hits different"},
+  { src: "https://i.imgur.com/t8vrzk3.jpeg", alt: "kronstad and sit @ 2am"},
+  { src: "https://i.imgur.com/5tY1gRg.jpeg", alt: "first time street tacos"},
+  { src: "https://i.imgur.com/vjX4FuH.png", alt: "so huge caaards"},
+  { src: "https://i.imgur.com/igWEt43.jpeg", alt: "vintage car with a friend"},
+  { src: "https://i.imgur.com/fXnoggX.jpeg", alt: "vintage car me"},
+  { src: "https://i.imgur.com/OEcVjJr.jpeg", alt: "2nd time climbing"},
+  { src: "https://i.imgur.com/DM3uKIQ.jpeg", alt: "dinner is served"},
+  { src: "https://i.imgur.com/aWzYQO8.jpeg", alt: "sit on the road and smile"},
+  { src: "https://i.imgur.com/jCe79ie.jpeg", alt: "met norwayrob!!!"},
+  { src: "https://i.imgur.com/UQN5Msc.jpeg", alt: "sunset by the sea"},
+  { src: "https://i.imgur.com/ub1iv7w.jpeg", alt: "kronstad @ 4am"},
+  { src: "https://i.imgur.com/BRCVsTd.png", alt: "cinematic foggy morning"},
+  { src: "https://i.imgur.com/I58fmwP.png", alt: "subic international airport"},
   { src: "https://i.imgur.com/8fwLAsa.jpeg", alt: "at subic bay"},
   { src: "https://i.imgur.com/ceBR7M6.jpeg", alt: "at an aviation school"},
   { src: "https://i.imgur.com/ABDdjqt.jpeg", alt: "riding a horse in my home city"},
