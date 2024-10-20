e53fa8a58b327504c6ede6cfc3ecac14efb6d549
nheek <nickjameshipol@gmail.com>
2024-10-20 14:13:32 +0200

    added archive, subs, and pasty to utils featured projects

--- components/FooterHero.tsx
+++ components/FooterHero.tsx
@@ -32,7 +32,7 @@ export default function FooterHero() {
         </div>
       </section>
       <a
-        className="w-1/4 block bg-[#1C2951] mx-auto text-center text-2xl brightness-125 hover:brightness-[unset] hover:-rotate-6 mt-4 p-4 rounded-3xl hover:bg-gray-200 hover:text-blue-950 duration-500 !no-underline"
+        className="w-[90%] md:w-1/4 block bg-[#1C2951] mx-auto text-center text-2xl brightness-125 hover:brightness-[unset] hover:-rotate-6 mt-10 md:mt-4 p-4 rounded-3xl hover:bg-gray-200 hover:text-blue-950 duration-500 !no-underline"
         href="mailto:nickjameshipol@gmail.com"
         target="_blank"
       >
