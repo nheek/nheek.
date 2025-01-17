497a89ae05911f82906a6e8c71a3b7706dc46c44
Nick <83167470+nheek@users.noreply.github.com>
2025-01-17 14:41:06 +0100

    remove Layout and Main components, integrate their functionality directly into Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,28 @@
 import Head from "next/head";
-import Layout from "../components/Layout";
-import getTextsMap from "../components/utils/GetTextsMap";
+// import Layout from "../components/Layout";
+// import getTextsMap from "../components/utils/GetTextsMap";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+// import SocialLinks from "../components/SocialLinks";
+import Navigate from "../components/Navigate";
+import FooterHero from "../components/FooterHero";
+// import Skills from "../components/Skills";
+import FeaturedProjects from "../components/FeaturedProjects";
 
 export default function Home() {
-  const wwwNheekNo = {
-    sitename: "nheek no",
-  };
-  const wwwDefault = {
-    sitename: "nheek",
-  };
-  const domainPairs = {
-    "www.nheek.no": wwwNheekNo,
-    default: wwwDefault,
-  };
-  const textsMap = getTextsMap(domainPairs);
-
   return (
     <>
       <Head>
-        <title>{textsMap.sitename}</title>
+        <title>{"nheek"}</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Portfolio website of nheek" />
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
-            textsMap.sitename,
+            "nheek",
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
-        <meta name="og:title" content={textsMap.sitename} />
+        <meta name="og:title" content={"nheek"} />
         <meta name="twitter:card" content="summary_large_image" />
         <script
           defer
@@ -35,7 +30,15 @@ export default function Home() {
           data-website-id="74e002f4-f2a6-43b6-9045-840f9632e53a"
         ></script>
       </Head>
-      <Layout />
+      <main>
+        <Hero />
+        {/* <SocialLinks /> */}
+        <FeaturedProjects />
+        {/* <Skills /> */}
+        <FooterHero />
+        <Navigate />
+      </main>
+      <Footer />
     </>
   );
 }
--- public/poems/belong.md
+++ public/poems/belong.md
@@ -8,36 +8,36 @@ date: June 08, 2018
 I’ll stay away,  
 Gone to another place,  
 Far away from home,  
-Never seen places before.  
+Never seen places before.
 
 Give me time,  
 Hold the distance,  
 I may not be there,  
-But you’re always on my mind.  
+But you’re always on my mind.
 
 The guitar plays a tune,  
 The wind rushes around,  
 My heart beats like a drum,  
-And the moon covers the night.  
+And the moon covers the night.
 
 Still, my mind keeps thinking,  
 Are you doing fine back home?  
-Or do you need someone to carry you on?  
+Or do you need someone to carry you on?
 
 Then he talks a wide small talk,  
 We said the past is a long way back,  
 How years pass and we didn’t see each other,  
-We both knew nothing could change now.  
+We both knew nothing could change now.
 
 And they’re down calling us for dinner,  
 But we still talk and knew each other,  
 Then we get down and eat our food,  
-I see how you’ve all grown up a whole.  
+I see how you’ve all grown up a whole.
 
 Though a decade passed that I didn’t meet you,  
 I knew how we knew we could still do  
 The things that were not meant for us,  
-Then cried inside about the truth tonight.  
+Then cried inside about the truth tonight.
 
 You all were smiling like nothing’s wrong,  
-And we all got full, and I felt I really belong.  
+And we all got full, and I felt I really belong.
